e56f6c9b977a861541e3b0b5070628aa73f7937a
divyansh2453 <ss7389181@gmail.com>
2026-04-22 21:56:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -66,14 +66,15 @@ posList = load_positions()
 MIN_THRESHOLD = 350
 
 #----------------------Hide Streamlit footer----------------------------
-hide_st_style = """
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
-#MainMenu {visibility: hidden;}
-footer {visibility: hidden;}
-header {visibility: hidden;}
-
-"""
-st.markdown(hide_st_style, unsafe_allow_html=True)
 #--------------------------------------------------------------------
 
 
